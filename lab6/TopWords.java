3d28067f4878d5a4a9b36fa8eb5dc023419e4922
MAnastasiaS <126902576+MAnastasiaS@users.noreply.github.com>
2023-12-31 01:58:14 +0300

    Create TopWords.java

---
+++ lab6/TopWords.java
@@ -0,0 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class TopWords {
+    public static void main(String[] args) {
+
+        File currentFile = new File("/Users/anna/IdeaProjects/laba6-7/src/tas1.txt");
+        String word = "";
+        Map<String, Integer> map = new HashMap<>();
+        try(Scanner scanner = new Scanner(currentFile)) {
+            while (scanner.hasNext()) {
+                word = scanner.next();
+                word = word.replaceAll("[^A-Za-zА-Яа-я0-9ёЁ]", "").trim().toLowerCase();
+                if (map.containsKey(word)) {
+                    map.put(word, map.get(word) + 1);
+                } else {
+                    map.put(word, 1);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        List<Integer> count = new ArrayList<>(map.values());
+        Collections.sort(count);
+        int i =0;
+        while (i<10){
+            for(String key: map.keySet()){
+                if(map.get(key).equals(count.get(count.size()-1))){
+                    System.out.println(key);
+                    map.remove(key);
+                    i++;
+                    count.remove(count.size()-1);
+                    break;
+                }
+            }
+        }
+
+    }
+}
