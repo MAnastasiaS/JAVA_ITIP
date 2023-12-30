cf22ab0f800f406df910a58be7d3fb66d934d055
MAnastasiaS <126902576+MAnastasiaS@users.noreply.github.com>
2023-12-31 01:56:39 +0300

    Create SaleItem.java

---
+++ lab6/SaleItem.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class SaleItem {
+    private String name;
+    private double price;
+
+    public SaleItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
